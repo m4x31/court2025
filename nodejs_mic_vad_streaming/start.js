96c2e4985d0bc6257caa477b5d63c84b2696eea0
Max Engel <max@maxengel.de>
2021-03-12 11:30:17 +0100

    remove speaker functionality from vad streaming project to fix broken windows dependencies

--- nodejs_mic_vad_streaming/start.js
+++ nodejs_mic_vad_streaming/start.js
@@ -3,7 +3,7 @@ const VAD = require('node-vad');
 const mic = require('mic');
 const fs = require('fs');
 const wav = require('wav');
-const Speaker = require('speaker');
+// const Speaker = require('speaker');
 
 let DEEPSPEECH_MODEL; // path to deepspeech model directory
 if (process.env.DEEPSPEECH_MODEL) {
@@ -235,24 +235,24 @@ function onRecognize(results) {
 	}
 }
 
-if (process.argv[2]) {
-	// if an audio file is supplied as an argument, play through the speakers to be picked up by the microphone
-	console.log('play audio file', process.argv[2]);
-	var file = fs.createReadStream(process.argv[2]);
-	var reader = new wav.Reader();
-	reader.on('format', function (format) {
-		firstChunkVoice = true;   // override vad for this test
-		SILENCE_THRESHOLD = 1000; // override silence (debounce time)
-		startMicrophone(function(results) {
-			console.log(results);
-			process.exit();
-		});
-		setTimeout(function() {
-			reader.pipe(new Speaker(format));
-		},900);
-	});
-	file.pipe(reader);
-}
-else {
+// if (process.argv[2]) {
+// 	// if an audio file is supplied as an argument, play through the speakers to be picked up by the microphone
+// 	console.log('play audio file', process.argv[2]);
+// 	var file = fs.createReadStream(process.argv[2]);
+// 	var reader = new wav.Reader();
+// 	reader.on('format', function (format) {
+// 		firstChunkVoice = true;   // override vad for this test
+// 		SILENCE_THRESHOLD = 1000; // override silence (debounce time)
+// 		startMicrophone(function(results) {
+// 			console.log(results);
+// 			process.exit();
+// 		});
+// 		setTimeout(function() {
+// 			reader.pipe(new Speaker(format));
+// 		},900);
+// 	});
+// 	file.pipe(reader);
+// }
+// else {
 	startMicrophone(onRecognize);
-}
+// }
